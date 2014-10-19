0b4707be417454c23dcda52225a38e13336c6db4
Roger Chen <roger@rogerhub.com>
2014-10-19 14:17:39 -0700

    Added rdesktop

--- modules/people/manifests/rogerhub.pp
+++ modules/people/manifests/rogerhub.pp
@@ -21,7 +21,7 @@ class people::rogerhub {
   }
 
   package {
-    ['rdiff-backup', 'awscli', 'fswatch', 'go', 'ctags', 'the_silver_searcher']:
+    ['rdiff-backup', 'awscli', 'fswatch', 'go', 'ctags', 'the_silver_searcher', 'rdesktop']:
       ensure => installed,
       provider => 'homebrew';
     ['gpgtools', 'macvim', 'keepassx', 'gnucash', 'google-hangouts', 'vlc', 'inkscape', 'xquartz', 'calibre', 'basictex', 'texshop', 'caffeine', 'nmap']:
